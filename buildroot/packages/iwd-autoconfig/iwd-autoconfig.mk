420141d3b615045ccd0c914e638a7ccf65640d6c
Miguel Fuertes <mjfuertesf@gmail.com>
2025-10-26 11:33:29 +0100

    fixed auto wifi

--- buildroot/packages/iwd-autoconfig/iwd-autoconfig.mk
+++ buildroot/packages/iwd-autoconfig/iwd-autoconfig.mk
@@ -8,8 +8,8 @@ IWD_AUTOCONFIG_VERSION = 1.0
 IWD_AUTOCONFIG_LICENSE = MIT
 
 define IWD_AUTOCONFIG_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_IWD_AUTOCONFIG_PATH)/package/iwd-autoconfig/S40iwd-autoconfig \
-		$(TARGET_DIR)/etc/init.d/S40iwd-autoconfig
+	$(INSTALL) -D -m 0755 $(@D)/S39iwd-autoconfig \
+		$(TARGET_DIR)/etc/init.d/S39iwd-autoconfig
 endef
 
 $(eval $(generic-package))
