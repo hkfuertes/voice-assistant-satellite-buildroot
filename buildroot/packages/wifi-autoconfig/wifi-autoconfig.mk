e852dd75c36025e248abd0e249763a59d8318339
Miguel Fuertes <mjfuertesf@gmail.com>
2025-10-27 00:46:44 +0100

    recovered diffconfig

--- buildroot/packages/wifi-autoconfig/wifi-autoconfig.mk
+++ buildroot/packages/wifi-autoconfig/wifi-autoconfig.mk
@@ -6,26 +6,35 @@
 
 WIFI_AUTOCONFIG_VERSION = 1.0
 WIFI_AUTOCONFIG_LICENSE = MIT
-
-# No source needed - just an init script
 WIFI_AUTOCONFIG_SOURCE =
 
-define WIFI_AUTOCONFIG_INSTALL_INIT_SYSV
+define WIFI_AUTOCONFIG_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/etc/modprobe.d
 	echo "options brcmfmac roamoff=1 feature_disable=0x82000" > \
 		$(TARGET_DIR)/etc/modprobe.d/brcmfmac.conf
-
+	
 	sed -i 's/^#*PermitRootLogin.*/PermitRootLogin yes/' \
 		$(TARGET_DIR)/etc/ssh/sshd_config
 	sed -i 's/^#*PasswordAuthentication.*/PasswordAuthentication yes/' \
 		$(TARGET_DIR)/etc/ssh/sshd_config
-
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_CUSTOM_PACKAGES_PATH)/wifi-autoconfig/files/S39wifi-autoconfig \
-		$(TARGET_DIR)/etc/init.d/S39wifi-autoconfig
 	
 	test -f $(BR2_EXTERNAL_CUSTOM_PACKAGES_PATH)/wifi-autoconfig/files/wpa_supplicant.conf && \
 		$(INSTALL) -D -m 0600 $(BR2_EXTERNAL_CUSTOM_PACKAGES_PATH)/wifi-autoconfig/files/wpa_supplicant.conf \
 		$(TARGET_DIR)/etc/wpa_supplicant.conf || true
 endef
+
+define WIFI_AUTOCONFIG_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_CUSTOM_PACKAGES_PATH)/wifi-autoconfig/files/S39wifi-autoconfig \
+		$(TARGET_DIR)/etc/init.d/S39wifi-autoconfig
+endef
+
+define WIFI_AUTOCONFIG_POST_IMAGE_FIXUP
+	if [ -f $(BINARIES_DIR)/rpi-firmware/config.txt ]; then \
+		sed -i '/^gpu_mem.*=/d' $(BINARIES_DIR)/rpi-firmware/config.txt && \
+		echo "gpu_mem=64" >> $(BINARIES_DIR)/rpi-firmware/config.txt; \
+	fi
+endef
+
+WIFI_AUTOCONFIG_TARGET_FINALIZE_HOOKS += WIFI_AUTOCONFIG_POST_IMAGE_FIXUP
 
 $(eval $(generic-package))
