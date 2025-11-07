1117bb2b0eff30a30e255c58dfba4fdf5173fae4
Miguel Fuertes <mjfuertesf@gmail.com>
2025-11-08 00:04:11 +0100

    refactor: update python-pymicro package configurations and add missing files

--- buildroot/external/package/python-pymicro-wakeword/python-pymicro-wakeword.mk
+++ buildroot/external/package/python-pymicro-wakeword/python-pymicro-wakeword.mk
@@ -4,13 +4,29 @@
 #
 ################################################################################
 
-PYTHON_PYMICRO_WAKEWORD_VERSION = 2.1.0
+PYTHON_PYMICRO_WAKEWORD_VERSION = v2.1.0
+PYTHON_PYMICRO_WAKEWORD_SITE = https://github.com/OHF-Voice/pymicro-wakeword.git
+PYTHON_PYMICRO_WAKEWORD_SITE_METHOD = git
+PYTHON_PYMICRO_WAKEWORD_SETUP_TYPE = setuptools
 PYTHON_PYMICRO_WAKEWORD_LICENSE = Apache-2.0
-PYTHON_PYMICRO_WAKEWORD_SOURCE = 
+PYTHON_PYMICRO_WAKEWORD_LICENSE_FILES = LICENSE
 
-define PYTHON_PYMICRO_WAKEWORD_INSTALL_TARGET_CMDS
-	unzip -o -d $(TARGET_DIR)/usr/lib/python3.13/site-packages/ \
-		$(PYTHON_PYMICRO_WAKEWORD_PKGDIR)/pymicro_wakeword-$(PYTHON_PYMICRO_WAKEWORD_VERSION)-py3-none-manylinux_2_35_aarch64.whl
+PYTHON_PYMICRO_WAKEWORD_DEPENDENCIES = python-numpy tensorflow-lite
+
+define PYTHON_PYMICRO_WAKEWORD_PRE_BUILD_TARGET_COPY_DEBUG
+	if [ -d "$(PYTHON_PYMICRO_WAKEWORD_DIR)/debug" ]; then \
+		cp -rv $(PYTHON_PYMICRO_WAKEWORD_DIR)/debug/* $(@D)/ ; \
+	fi
 endef
 
-$(eval $(generic-package))
+define PYTHON_PYMICRO_WAKEWORD_POST_INSTALL_MAKE_LNS
+    mkdir -p $(TARGET_DIR)/usr/lib/python3.13/site-packages/pymicro_wakeword/lib
+    ln -sf /usr/lib/libtensorflow-lite.so \
+        $(TARGET_DIR)/usr/lib/python3.13/site-packages/pymicro_wakeword/lib/libtensorflowlite_c.so
+endef
+
+
+PYTHON_PYMICRO_WAKEWORD_PRE_BUILD_TARGET_HOOKS += PYTHON_PYMICRO_WAKEWORD_PRE_BUILD_TARGET_COPY_DEBUG
+PYTHON_PYMICRO_WAKEWORD_POST_INSTALL_TARGET_HOOKS += PYTHON_PYMICRO_WAKEWORD_POST_INSTALL_MAKE_LNS
+
+$(eval $(python-package))
