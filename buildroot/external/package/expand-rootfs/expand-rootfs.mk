601ff8f0a235dd0742c20863707921d99d89ba36
Miguel Fuertes <mjfuertesf@gmail.com>
2025-11-14 11:27:40 +0100

    update: standardize source path references in package makefiles

--- buildroot/external/package/expand-rootfs/expand-rootfs.mk
+++ buildroot/external/package/expand-rootfs/expand-rootfs.mk
@@ -6,16 +6,14 @@
 
 EXPAND_ROOTFS_VERSION = 1.0
 EXPAND_ROOTFS_LICENSE = MIT
-EXPAND_ROOTFS_SOURCE =
+EXPAND_ROOTFS_SOURCE = $(BR2_EXTERNAL_CUSTOM_PACKAGES_PATH)/package/expand-rootfs/files
 
 define EXPAND_ROOTFS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_CUSTOM_PACKAGES_PATH)/package/expand-rootfs/files/rootfs-resizer \
-		$(TARGET_DIR)/usr/bin/rootfs-resizer
+	$(INSTALL) -D -m 0755 $(@D)/rootfs-resizer $(TARGET_DIR)/usr/bin/rootfs-resizer
 endef
 
 define EXPAND_ROOTFS_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_CUSTOM_PACKAGES_PATH)/package/expand-rootfs/files/S01expand-rootfs \
-		$(TARGET_DIR)/etc/init.d/S01expand-rootfs
+	$(INSTALL) -D -m 0755 $(@D)/S01expand-rootfs $(TARGET_DIR)/etc/init.d/S01expand-rootfs
 endef
 
 $(eval $(generic-package))
--- buildroot/external/package/wifi-autoconfig/wifi-autoconfig.mk
+++ buildroot/external/package/wifi-autoconfig/wifi-autoconfig.mk
@@ -6,20 +6,20 @@
 
 WIFI_AUTOCONFIG_VERSION = 1.0
 WIFI_AUTOCONFIG_LICENSE = MIT
-WIFI_AUTOCONFIG_SOURCE =
+WIFI_AUTOCONFIG_SOURCE = $(BR2_EXTERNAL_CUSTOM_PACKAGES_PATH)/package/wifi-autoconfig/files
 
 define WIFI_AUTOCONFIG_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/etc/modprobe.d
 	echo "options brcmfmac roamoff=1 feature_disable=0x82000" > \
 		$(TARGET_DIR)/etc/modprobe.d/brcmfmac.conf
 	
-	test -f $(BR2_EXTERNAL_CUSTOM_PACKAGES_PATH)/package/wifi-autoconfig/files/wpa_supplicant.conf && \
-		$(INSTALL) -D -m 0600 $(BR2_EXTERNAL_CUSTOM_PACKAGES_PATH)/package/wifi-autoconfig/files/wpa_supplicant.conf \
+	test -f $(@D)/wpa_supplicant.conf && \
+		$(INSTALL) -D -m 0600 $(@d)/wpa_supplicant.conf \
 		$(TARGET_DIR)/etc/wpa_supplicant.conf || true
 endef
 
 define WIFI_AUTOCONFIG_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_CUSTOM_PACKAGES_PATH)/package/wifi-autoconfig/files/S39wifi-autoconfig \
+	$(INSTALL) -D -m 0755 $(@D)/S39wifi-autoconfig \
 		$(TARGET_DIR)/etc/init.d/S39wifi-autoconfig
 endef
 
